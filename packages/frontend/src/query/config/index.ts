4eb3faf0e75e2b2e6c16039b1bf64336c028bafd
weicanie <2042365244@qq.com>
2025-08-12 10:12:30 +0800

    fix: 登出时的react-query缓存遗留

--- packages/frontend/src/query/config/index.ts
+++ packages/frontend/src/query/config/index.ts
@@ -7,7 +7,8 @@ import {
 	type UseMutationResult,
 	type UseQueryOptions,
 	useMutation,
-	useQuery
+	useQuery,
+	useQueryClient
 } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
@@ -54,6 +55,7 @@ export function useCustomQuery<SD>( //SD: Response Data
 	options?: Partial<UseQueryOptions<SDF<SD>>>
 ) {
 	const navigate = useNavigate();
+	const client = useQueryClient();
 	//避免处理错误处理的select被覆盖
 	let outerSelect: ((data: SDF<SD>) => SDF<SD>) | undefined;
 	if (options?.select) {
@@ -63,9 +65,7 @@ export function useCustomQuery<SD>( //SD: Response Data
 		queryKey,
 		queryFn,
 		staleTime: 1 * 60 * 1000, //1分钟后过时
-		retry: failureCount => {
-			return failureCount < 2;
-		},
+		retry: false,
 		...options,
 		/* 统一的错误处理	 
       服务器始终会响应数据,通过code来判断错误
@@ -76,10 +76,18 @@ export function useCustomQuery<SD>( //SD: Response Data
 				if (data.code === '2006' || data.code === '2007') {
 					localStorage.removeItem('token');
 					localStorage.removeItem('userInfo');
-					addToastToQueue(
-						data.code === '2006' ? '登录已过期，请重新登录' : '请先登录',
-						ToastType.Warning
-					);
+					if (!data.message || data.message == DEFAULT_MESSAGE) {
+						addToastToQueue(
+							data.code === '2006' ? '登录已过期，请重新登录' : '请先登录',
+							ToastType.Warning
+						);
+					}
+					/**
+					 * 清除之前用户的所有缓存。
+					 * 注意：因为后端API统一返回结果，因此错误也会被缓存，这会导致token过期失效后卡在登录页面——不断获得缓存中的未登录错误，从而不断删除获得的token并请求登录。
+					 * 缓存未登录错误的接口难以确定，通过清除所有缓存来解决此问题才是正确的，因为换号登录本该让所有缓存失效，否则用户获取到的是上一登录账户的数据。
+					 */
+					client.clear();
 					navigate('/login');
 				} else {
 					addToastToQueue(data.message || '系统繁忙，请稍后再试', ToastType.Error);
@@ -116,14 +124,13 @@ export function useCustomMutation<TData, TVariables, TContext = unknown>(
 	options?: Partial<UseMutationOptions<SDF<TData>, unknown, TVariables, TContext>>
 ): UseMutationResult<SDF<TData>, unknown, TVariables, TContext> {
 	const navigate = useNavigate();
+	const client = useQueryClient();
 	//避免处理错误处理的select被覆盖
 	const outerOnSuccess = options?.onSuccess;
 	const outerOnError = options?.onError;
 	return useMutation({
 		// 重试逻辑
-		retry: failureCount => {
-			return failureCount < 2; // 最多试2次
-		},
+		retry: false,
 		retryDelay: attemptCount => {
 			return Math.min(1000 * 2 ** attemptCount, 30000); // 最长延迟30秒
 		},
@@ -137,15 +144,18 @@ export function useCustomMutation<TData, TVariables, TContext = unknown>(
 				if (data.code === '2006' || data.code === '2007') {
 					localStorage.removeItem('token');
 					localStorage.removeItem('userInfo');
-					addToastToQueue(
-						data.code === '2006' ? '登录已过期，请重新登录' : '请先登录',
-						ToastType.Warning
-					);
+					if (!data.message || data.message == DEFAULT_MESSAGE) {
+						addToastToQueue(
+							data.code === '2006' ? '登录已过期，请重新登录' : '请先登录',
+							ToastType.Warning
+						);
+					}
+					client.clear();
 					navigate('/login');
 					return;
 				} else {
 					addToastToQueue(data.message || '系统繁忙，请稍后再试', ToastType.Error);
-					throw new Error(data.message);
+					throw new Error(data.message); //触发组件设置的onError回调
 				}
 			} else {
 				/* 返回默认的'ok'时，不显示toast，交给外部组件来决定toast */
@@ -158,10 +168,9 @@ export function useCustomMutation<TData, TVariables, TContext = unknown>(
 			}
 		},
 
-		// 处理其它错误（如网络错误）
+		// 触发组件设置的onError回调
 		onError: (error, variables, context) => {
-			addToastToQueue(`系统繁忙，请稍后再试${error}`, ToastType.Error);
-
+			// addToastToQueue(`系统繁忙，请稍后再试${error}`, ToastType.Error);
 			if (outerOnError) {
 				outerOnError(error, variables, context);
 			}
