be1581b17cd022bfe1bd23bd4230dd4f2dbbe107
weicanie <2042365244@qq.com>
2025-09-16 10:49:53 +0800

    fix: 后端容器依赖

--- packages/backend/src/copilot/copilotkit.controller.ts
+++ packages/backend/src/copilot/copilotkit.controller.ts
@@ -1,4 +1,4 @@
-import { CopilotRuntime, copilotRuntimeNestEndpoint, LangChainAdapter } from '@copilotkit/runtime';
+// import { CopilotRuntime, copilotRuntimeNestEndpoint, LangChainAdapter } from '@copilotkit/runtime';
 import { All, Controller, Req, Res } from '@nestjs/common';
 import { type Request, type Response } from 'express';
 import { ModelService } from '../model/model.service';
@@ -8,21 +8,21 @@ export class CopilotkitController {
 	constructor(public modelService: ModelService) {}
 	@All('/copilotkit')
 	copilotkit(@Req() req: Request, @Res() res: Response) {
-		const serviceAdapter = new LangChainAdapter({
-			chainFn: async ({ messages, tools }) => {
-				const model = await this.modelService.getLLMDeepSeekRaw('deepseek-chat');
-				return model.bindTools(tools).stream(messages);
-				// or optionally enable strict mode
-				// return model.bindTools(tools, { strict: true }).stream(messages);
-			}
-		});
-		const runtime = new CopilotRuntime();
+		return 'not implemented';
+		// const serviceAdapter = new LangChainAdapter({
+		// 	chainFn: async ({ messages, tools }) => {
+		// 		const model = await this.modelService.getLLMDeepSeekRaw('deepseek-chat');
+		// 		return model.bindTools(tools).stream(messages);
 
-		const handler = copilotRuntimeNestEndpoint({
-			runtime,
-			serviceAdapter,
-			endpoint: '/copilotkit'
-		});
-		return handler(req, res);
+		// 	}
+		// });
+		// const runtime = new CopilotRuntime();
+
+		// const handler = copilotRuntimeNestEndpoint({
+		// 	runtime,
+		// 	serviceAdapter,
+		// 	endpoint: '/copilotkit'
+		// });
+		// return handler(req, res);
 	}
 }
