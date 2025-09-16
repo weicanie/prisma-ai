7bdc377b508106c8e0275018ae97935b2d0eb96b
weicanie <2042365244@qq.com>
2025-09-16 16:14:07 +0800

    build: 简历编辑器镜像的构建
    实现简历编辑器镜像在monorepo中的构建

--- packages/magic-resume/next.config.mjs
+++ packages/magic-resume/next.config.mjs
@@ -1,13 +1,24 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
+
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// // ESM 环境下获取 __dirname 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const config = {
 	typescript: {
 		ignoreBuildErrors: true
 	},
-	output: 'standalone',
+	/* monorepo中的standalone要么不可用，要么需要额外配置！！！ */
+	// output: 'standalone',
+	// 不起作用：
+	// experimental: {
+	// 	// 指定输出文件追踪的根目录为 monorepo 根目录
+	// 	outputFileTracingRoot: path.resolve(__dirname, '../../'),
+	// },
 	/* 微前端 */
 	// 解决主应用和微应用通信时的跨域问题
 	async headers() {
