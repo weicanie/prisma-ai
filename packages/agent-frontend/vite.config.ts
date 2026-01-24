4513c72c32b95fd39632d37685467c33b90da053
weicanie <2042365244@qq.com>
2026-01-25 03:33:08 +0800

    ci(deployment): configure agent-frontend for nginx deployment

--- packages/agent-frontend/vite.config.ts
+++ packages/agent-frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/agent_frontend/',
 	plugins: [vue(), vueDevTools(), tailwindcss()],
 	resolve: {
 		alias: {
